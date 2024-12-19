d57a396f51e47fe757663d72a5de3bcb4943c030
Bareman07 <146845508+Bareman07@users.noreply.github.com>
2024-12-19 15:16:52 +0100

    startServices

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -3,13 +3,42 @@
 // Page de blog
 const Blog = () => {
   return (
-    <main>
+    <main className="MainBlog d-flex flex-column justify-content-center align-items-center">
       {/* Titre principal de la page */}
       <h1>Blog</h1>
-      {/* Introduction au contenu du blog */}
+      {/* Texte décrivant le contenu du Blog */}
       <p>Bienvenue sur mon blog. Découvrez mes articles sur le développement web et les technologies modernes.</p>
+      {/* Conteneur flex pour centrer les cartes */}
+      <div className="ContenairBlog d-flex justify-content-center"r style={{ width: '100%' }}>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card1 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card2 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card3 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+      </div>
+      <div className="ContenairBlog d-flex justify-content-center"r style={{ width: '100%' }}>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card1 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card2 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card3 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+      </div>
     </main>
-  );
+  )
 };
 
 export default Blog;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -3,11 +3,26 @@
 // Page des services proposés
 const Services = () => {
   return (
-    <main>
+    <main className="MainServices d-flex flex-column justify-content-center align-items-center">
       {/* Titre principal de la page */}
       <h1>Services</h1>
-      {/* Liste ou description des services */}
-      <p>Je propose des services de développement web, de conception UI/UX, et d&aposoptimisation SEO.</p>
+      {/* Texte décrivant le contenu du Services */}
+      <p>Je propose des services de développement web, de conception UI/UX, et d&apos;optimisation SEO.</p>
+      {/* Conteneur flex pour centrer les cartes */}
+      <div className="ContenairServices d-flex justify-content-center"r style={{ width: '100%' }}>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card1 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card2 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+        <div className="card mx-4 my-4" style={{ width: '18rem' }}>
+            {/* Image de la card3 */}
+            <img src="https://via.placeholder.com/150" className="card-img-top" alt="Image de la card" />
+        </div>
+      </div>
     </main>
   );
 };
